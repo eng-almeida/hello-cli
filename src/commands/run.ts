e392aa00eb3cdbb6708ae12157e7607cc2364de2
Joao Almeida <joao.almeida@remote.com>
2023-10-22 10:51:08 +0100

    throw when cli parameters or env vars equivalent don't exist

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -2,6 +2,8 @@ import { Command, Flags } from '@oclif/core'
 // eslint-disable-next-line unicorn/prefer-node-protocol
 import { spawn } from 'child_process';
 
+const assert = require('node:assert');
+
 export default class Run extends Command {
   static description = 'describe the command here'
 
@@ -20,6 +22,16 @@ export default class Run extends Command {
   public async run(): Promise<void> {
     const { flags } = await this.parse(Run);
 
+    const organizationId = flags?.organizationId || process.env.NOCTUA_ORGANIZATION_ID;
+    assert(organizationId, 'Organization id was not provided');
+    
+    const projectId = flags?.projectId || process.env.NOCTUA_PROJECT_ID;
+    assert(projectId, 'Project id was not provided');
+
+    const secretKey = flags?.organizationId || process.env.NOCTUA_SECRET_KEY;
+    assert(secretKey, 'Secret key was not provided. Read docs about to get your Noctua secret');
+
+
     const dangerCommand = 'danger';
     const dangerArgs = [
       'ci',
@@ -27,13 +39,15 @@ export default class Run extends Command {
       'src/lib/core/index.ts'
     ]
     const envVariables = {
+      // ------- TO REMOVE START ------------
       DANGER_GITHUB_API_TOKEN: '',
       DANGER_FAKE_CI: 'true',
-      DANGER_TEST_REPO: 'eng-almeida/gh-action',
-      DANGER_TEST_PR: '2',
-      NOCTUA_ORGANIZATION_ID: flags?.organizationId || process.env.NOCTUA_ORGANIZATION_ID || 'remote-123',
-      NOCTUA_PROJECT_ID: flags?.projectId || process.env.NOCTUA_PROJECT_ID || 'dragon-project-123',
-      NOCTUA_SECRET_KEY: flags?.organizationId || process.env.NOCTUA_SECRET_KEY || 'clnsvv1a80004k1pksa4l2a6n'
+      DANGER_TEST_REPO: '',
+      DANGER_TEST_PR: '',
+      // ------- TO REMOVE END ------------
+      NOCTUA_ORGANIZATION_ID: organizationId,
+      NOCTUA_PROJECT_ID: projectId,
+      NOCTUA_SECRET_KEY: secretKey
     };
 
     const danger = spawn(dangerCommand, dangerArgs, {
